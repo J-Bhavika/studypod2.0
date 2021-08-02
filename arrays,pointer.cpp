04a2f083342af608b9e27778617c255f0fdc7f25
J-Bhavika <87748562+J-Bhavika@users.noreply.github.com>
2021-08-02 13:18:50 +0530

    Update arrays,pointer.cpp

--- arrays,pointer.cpp
+++ arrays,pointer.cpp
@@ -12,7 +12,7 @@ cout<<"the value of marks[0] is"<<*p<<endl;
 cout<<"the value of marks[1] is"<<*(p+1)<<endl;
 cout<<"the value of marks[2] is"<<*(p+2)<<endl;
 cout<<"the value of marks[3] is"<<*(p+3)<<endl;
-
+// the above can be written like this too:-
 //cout<<"the value of *p is"<<*(p+3)<<endl;
 //cout<<"the value of *(p+1) is"<<*(p+3)<<endl;
 //cout<<"the value of *(p+2) is"<<*(p+3)<<endl;
