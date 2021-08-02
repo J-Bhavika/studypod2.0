27c4e6c77d57b0e20349c6a8be60b98c5347ef5b
J-Bhavika <87748562+J-Bhavika@users.noreply.github.com>
2021-08-02 13:07:44 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+int marks[4]={12,24,36,48};
+int i=0;
+do{
+        cout<<"the value of marks"<<i<<"is"<<marks[i]<<endl;
+ i++;
+ }while(i<=3);
+ return 0;
+}
---
+++ arrays,pointer.cpp
@@ -0,0 +1,27 @@
+/*pointer and arrays
+pointer arithmetic
+address new= address current + i*size of datatype*/
+#include<iostream>
+using namespace std;
+int main(){
+int marks[]={23,45,56,89};
+int*p=marks;
+marks[2]=78;
+//we can change the value
+cout<<"the value of marks[0] is"<<*p<<endl;
+cout<<"the value of marks[1] is"<<*(p+1)<<endl;
+cout<<"the value of marks[2] is"<<*(p+2)<<endl;
+cout<<"the value of marks[3] is"<<*(p+3)<<endl;
+
+//cout<<"the value of *p is"<<*(p+3)<<endl;
+//cout<<"the value of *(p+1) is"<<*(p+3)<<endl;
+//cout<<"the value of *(p+2) is"<<*(p+3)<<endl;
+//cout<<"the value of *(p+3) is"<<*(p+3)<<endl;
+
+cout<<*(p++)<<endl;
+cout<<*(p)<<endl;
+
+cout<<*p++<<endl;
+cout<<*++p<<endl;
+return 0;
+}
